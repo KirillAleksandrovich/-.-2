1c44b4b1af9e09bc7676f4b9b00b9f78b1cbb87b
«Kirill» <kirill.feshev@bk.ru>
2022-09-08 23:56:06 +0300

    Task13,15

---
+++ Task13/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 13: Напишите программу, 
+// которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Clear();
+Console.WriteLine("Введи число: ");
+int random = Convert.ToInt32(Console.ReadLine());
+string randomNum = Convert.ToString(random);
+if (randomNum.Length > 2)
+{
+    Console.WriteLine("третья цифра -> " + randomNum[2]);
+}
+else
+{
+    Console.Write("-> третьей цифры нет");
+}
---
+++ Task15/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 15: Напишите программу, 
+// которая принимает на вход цифру, 
+// обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+Console.Clear();
+Console.Write("Введи цифру, обозначающую день недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void DaysOfTheWeek(int dayNumber)
+{
+    if (dayNumber == 6 || dayNumber == 7)
+    {
+        Console.WriteLine("(Выходной, иди отдыхай.) -> да");
+    }
+    else if (dayNumber < 1 || dayNumber > 7)
+    {
+        Console.WriteLine("Такого деня недели, не существует");
+    }
+    else Console.WriteLine("(Это не выходной день!) -> нет");
+}
+
+DaysOfTheWeek(dayNumber);
