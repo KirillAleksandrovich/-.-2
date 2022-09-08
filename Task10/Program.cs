6e7a1aa5209ae42c84930e686065bce309ec3961
«Kirill» <kirill.feshev@bk.ru>
2022-09-08 23:26:51 +0300

    Task10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -5,3 +5,13 @@
 // 456 -> 5
 // 782 -> 8
 // 918 -> 1
+Console.Clear();
+Console.WriteLine("Привет!");
+Console.WriteLine("Давай знакомиться? Как твоё имя?");
+String username = Console.ReadLine();
+Console.WriteLine($"Хорошо, {username}!");
+Console.WriteLine($"{username}, если введёшь трёхзначное число, я покажу вторую цифру этого числа :)");
+Console.WriteLine($"Если готов {username}, то вводи цифру: ");
+int threeDigit = Convert.ToInt32(Console.ReadLine());
+string stringNum = Convert.ToString(threeDigit);
+Console.WriteLine("вторая цифра этого числа -> " + stringNum[1]);
